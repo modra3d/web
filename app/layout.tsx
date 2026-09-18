62da54177a1a37f14d9fef0145cf19da2024e86f
modra3d <modra3d@gmail.com>
2026-09-18 06:52:11 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Modra 3D',
+  title: 'Modra 3D AI',
   description: 'Modra 3D',
   generator: 'v0.app',
 }
